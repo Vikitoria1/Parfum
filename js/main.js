9631a704fe584c5fad842e8eea9614c300a17c68
Viktoria Grushko <grushko13vika@gmail.com>
2024-06-28 23:03:36 +0300

    changes 5

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ const nav = document.querySelector('.header__inner');
 btn.addEventListener('click', ()=> {
     nav.classList.toggle('menu-open');
 });
+
+document.querySelectorAll('.answers__triger').forEach((item) =>{
+item.addEventListener('click', () => {
+    item.parentNode.classList.toggle('answers__item-active')
+  })
+});
+
